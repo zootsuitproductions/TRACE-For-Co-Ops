b29109c8cf18d0a488fb62a03d9708f66b6d2dea
markfontenot <m.fontenot@northeastern.edu>
2024-11-05 07:39:09 -0500

    Updated product routes docs

--- api/backend/products/products_routes.py
+++ api/backend/products/products_routes.py
@@ -3,17 +3,23 @@
 # Remove this file if you are not using it in your project
 ########################################################
 
-from flask import Blueprint, request, jsonify, make_response, current_app
-import json
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
 from backend.db_connection import db
 
-
+#------------------------------------------------------------
+# Create a new Blueprint object, which is a collection of 
+# routes.
 products = Blueprint('products', __name__)
 
-# Get all the products from the database
+#------------------------------------------------------------
+# Get all the products from the database, package them up,
+# and return them to the client
 @products.route('/products', methods=['GET'])
 def get_products():
-    
     query = '''
         SELECT  id, 
                 product_code, 
