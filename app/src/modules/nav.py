08a5d37c8b7c2dcc403c0514b4de87d27405b95a
Mark Fontenot <m.fontenot@northeastern.edu>
2024-11-12 07:00:01 -0500

    updated navigation docs

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -1,54 +1,75 @@
 # Idea borrowed from https://github.com/fsmosca/sample-streamlit-authenticator
 
+# This file has function to add certain functionality to the left side bar of the app
+
 import streamlit as st
+
 
 #### ------------------------ General ------------------------
 def HomeNav():
-    st.sidebar.page_link("Home.py", label="Home", icon='🏠')
+    st.sidebar.page_link("Home.py", label="Home", icon="🏠")
+
 
 def AboutPageNav():
     st.sidebar.page_link("pages/30_About.py", label="About", icon="🧠")
 
+
 #### ------------------------ Examples for Role of pol_strat_advisor ------------------------
 def PolStratAdvHomeNav():
-    st.sidebar.page_link("pages/00_Pol_Strat_Home.py", label="Political Strategist Home", icon='👤')
+    st.sidebar.page_link(
+        "pages/00_Pol_Strat_Home.py", label="Political Strategist Home", icon="👤"
+    )
+
 
 def WorldBankVizNav():
-    st.sidebar.page_link("pages/01_World_Bank_Viz.py", label="World Bank Visualization", icon='🏦')
+    st.sidebar.page_link(
+        "pages/01_World_Bank_Viz.py", label="World Bank Visualization", icon="🏦"
+    )
+
 
 def MapDemoNav():
-    st.sidebar.page_link("pages/02_Map_Demo.py", label="Map Demonstration", icon='🗺️')
+    st.sidebar.page_link("pages/02_Map_Demo.py", label="Map Demonstration", icon="🗺️")
+
 
 ## ------------------------ Examples for Role of usaid_worker ------------------------
 def ApiTestNav():
-    st.sidebar.page_link("pages/12_API_Test.py", label="Test the API", icon='🛜')
+    st.sidebar.page_link("pages/12_API_Test.py", label="Test the API", icon="🛜")
+
 
 def PredictionNav():
-    st.sidebar.page_link("pages/11_Prediction.py", label="Regression Prediction", icon='📈')
+    st.sidebar.page_link(
+        "pages/11_Prediction.py", label="Regression Prediction", icon="📈"
+    )
+
 
 def ClassificationNav():
-    st.sidebar.page_link("pages/13_Classification.py", label="Classification Demo", icon='🌺')
+    st.sidebar.page_link(
+        "pages/13_Classification.py", label="Classification Demo", icon="🌺"
+    )
+
 
 #### ------------------------ System Admin Role ------------------------
 def AdminPageNav():
-    st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon='🖥️')
-    st.sidebar.page_link("pages/21_ML_Model_Mgmt.py", label='ML Model Management', icon='🏢')
+    st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon="🖥️")
+    st.sidebar.page_link(
+        "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
+    )
 
 
 # --------------------------------Links Function -----------------------------------------------
 def SideBarLinks(show_home=False):
     """
-    This function handles adding links to the sidebar of the app based upon the logged-in user's role, which was put in the streamlit session_state object when logging in. 
-    """    
+    This function handles adding links to the sidebar of the app based upon the logged-in user's role, which was put in the streamlit session_state object when logging in.
+    """
 
     # add a logo to the sidebar always
-    st.sidebar.image("assets/logo.png", width = 150)
+    st.sidebar.image("assets/logo.png", width=150)
 
     # If there is no logged in user, redirect to the Home (Landing) page
-    if 'authenticated' not in st.session_state:
+    if "authenticated" not in st.session_state:
         st.session_state.authenticated = False
-        st.switch_page('Home.py')
-        
+        st.switch_page("Home.py")
+
     if show_home:
         # Show the Home page link (the landing page)
         HomeNav()
@@ -57,19 +78,19 @@ def SideBarLinks(show_home=False):
     if st.session_state["authenticated"]:
 
         # Show World Bank Link and Map Demo Link if the user is a political strategy advisor role.
-        if st.session_state['role'] == 'pol_strat_advisor':
+        if st.session_state["role"] == "pol_strat_advisor":
             PolStratAdvHomeNav()
             WorldBankVizNav()
             MapDemoNav()
 
         # If the user role is usaid worker, show the Api Testing page
-        if st.session_state['role'] == 'usaid_worker':
+        if st.session_state["role"] == "usaid_worker":
             PredictionNav()
-            ApiTestNav() 
+            ApiTestNav()
             ClassificationNav()
-        
+
         # If the user is an administrator, give them access to the administrator pages
-        if st.session_state['role'] == 'administrator':
+        if st.session_state["role"] == "administrator":
             AdminPageNav()
 
     # Always show the About page at the bottom of the list of links
@@ -78,7 +99,6 @@ def SideBarLinks(show_home=False):
     if st.session_state["authenticated"]:
         # Always show a logout button if there is a logged in user
         if st.sidebar.button("Logout"):
-            del st.session_state['role']
-            del st.session_state['authenticated']
-            st.switch_page('Home.py')
-
+            del st.session_state["role"]
+            del st.session_state["authenticated"]
+            st.switch_page("Home.py")
