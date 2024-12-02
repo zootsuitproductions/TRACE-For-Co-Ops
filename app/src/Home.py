15ef2a095e3a2632a2149319ef45aac1ed896e5e
sevensrig <srinivasan.sri@northeastern.edu>
2024-12-02 17:16:41 -0500

    Seb CoOp Searcher Button implemented

--- app/src/Home.py
+++ app/src/Home.py
@@ -48,14 +48,14 @@ if st.button("Act as Sebastian, a Student Co Op Searcher",
     # when user clicks the button, they are now considered authenticated
     st.session_state['authenticated'] = True
     # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
+    st.session_state['role'] = 'Co-Op searcher'
     # we add the first name of the user (so it can be displayed on 
     # subsequent pages). 
-    st.session_state['first_name'] = 'John'
+    st.session_state['first_name'] = 'Sebastian'
     # finally, we ask streamlit to switch to another page, in this case, the 
     # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/00_Pol_Strat_Home.py')
+    logger.info("Logging in as Co-Op Searcher")
+    st.switch_page('pages/100_CoOp_Searcher_Home.py')
 
 if st.button('Act as Riley, a student Co-Op Reviewer', 
             type = 'primary', 
@@ -63,7 +63,7 @@ if st.button('Act as Riley, a student Co-Op Reviewer',
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'usaid_worker'
     st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
+    st.switch_page('pages/100_CoOp_Searcher_Home.py')
 
 if st.button('Act as System Administrator', 
             type = 'primary', 
