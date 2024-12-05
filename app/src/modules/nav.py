e398f5f88cab2e0097bd58966cd52cced0fd647a
Ren Yin <147033895+R-Yin-217@users.noreply.github.com>
2024-12-05 01:24:23 -0500

    update nav.py

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -50,10 +50,7 @@ def ClassificationNav():
 
 #### ------------------------ System Admin Role ------------------------
 def AdminPageNav():
-    st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon="🖥️")
-    st.sidebar.page_link(
-        "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
-    )
+    st.sidebar.page_link("pages/300_System_Administrator_Home.py", label="System Admin", icon="🖥️")
 
 
 # --------------------------------Links Function -----------------------------------------------
