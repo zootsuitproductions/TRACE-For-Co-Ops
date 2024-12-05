4b7e37f1c5244d1980bf1ecf465e6b93788c9b4d
sevensrig <srinivasan.sri@northeastern.edu>
2024-12-05 16:21:28 -0500

    remove unneccesary imports

--- app/src/pages/2002_My_Reviews.py
+++ app/src/pages/2002_My_Reviews.py
@@ -1,12 +1,6 @@
 import logging
 logger = logging.getLogger(__name__)
-import pandas as pd
 import streamlit as st
-from streamlit_extras.app_logo import add_logo
-import world_bank_data as wb
-import matplotlib.pyplot as plt
-import numpy as np
-import plotly.express as px
 from modules.nav import SideBarLinks
 import requests
 
