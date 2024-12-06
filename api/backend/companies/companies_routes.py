7ee1eddaa730d5f1e4df40ab54058ead1ca02275
Ren Yin <147033895+R-Yin-217@users.noreply.github.com>
2024-12-06 00:53:10 -0500

    add company for admin

--- api/backend/companies/companies_routes.py
+++ api/backend/companies/companies_routes.py
@@ -22,13 +22,13 @@ def get_feedback():
     try:
         with db.get_db().cursor() as cursor:
             cursor.execute('''
-                SELECT C.name, C.description, C.updatedAT, I.name, L.address, L.city, L.state_province, L.country, R.roleName, R.description, R.skillsRequired
+                SELECT C.companyID, C.name, C.description, C.updatedAT, I.name, L.address, L.city, L.state_province, L.country, R.roleName, R.description, R.skillsRequired, L.locationID AS `Location ID`, I.IndustryID AS `Industry ID`, R.roleID AS `Role ID`
                 FROM Companies C JOIN Location L
                 ON C.companyID = L.companyID JOIN CompanyIndustry CI
                 ON CI.companyID = C.companyID JOIN Industries I
                 ON I.industryID = CI.industryID JOIN Role R
                 ON R.companyID = C.companyID
-                ORDER BY C.name ASC;
+                ORDER BY C.companyID, C.name ASC;
             ''')
             theData = cursor.fetchall()
 
@@ -40,42 +40,81 @@ def get_feedback():
         current_app.logger.error(f"Error fetching companies: {e}")
         return {"error": "An error occurred while fetching companies"}, 500
 
-'''
-@feedback.route('/feedback', methods=['PUT'])
-def update_status():
+
+@companies.route('/companies/companies', methods=['PUT'])
+def update_company():
     current_app.logger.info('PUT /feedback route')
     try:
-        feedback_info = request.json
+        companies_info = request.json
 
-        if 'feedbackID' not in feedback_info or 'status' not in feedback_info:
-            return {'error': 'Missing feedbackID or status'}, 400
+        if 'companyID' not in companies_info or 'Company Description' not in companies_info:
+            return {'error': 'Missing companyID or Company Description'}, 400
 
-        status = feedback_info['status']
-        feedback_id = feedback_info['feedbackID']
+        company_id = companies_info['companyID']
+        company_discription = companies_info['Company Description']
         
         
-        query = 
-        UPDATE Feedback
-        SET status = %s
-        WHERE feedbackID = %s;
+        query = '''
+        UPDATE Companies
+        SET description = %s
+        WHERE companyID = %s
+        '''
         
-        
-        
-        data = (status, feedback_id)
+        data = (company_discription, company_id)
 
         cursor = db.get_db().cursor()
         cursor.execute(query, data)
         db.get_db().commit()
 
-        return {'message': 'Feedback status updated successfully'}, 200
+        return {'message': 'Companies updated successfully'}, 200
 
     except KeyError as e:
         current_app.logger.error(f"Missing key in request JSON: {str(e)}")
         return {'error': f'Missing key: {str(e)}'}, 400
     except Exception as e:
         current_app.logger.error(f"Error updating feedback: {str(e)}")
-        return {'error': 'An error occurred while updating feedback status'}, 500
+        return {'error': 'An error occurred while updating companies'}, 500
     finally:
         if 'cursor' in locals() and cursor:
             cursor.close()
-'''
+            
+            
+@companies.route('/companies/roles', methods=['PUT'])
+def update_roles():
+    current_app.logger.info('PUT /feedback route')
+    try:
+        companies_info = request.json
+
+        if 'RoleID' not in companies_info or 'Skills Required' not in companies_info or 'Role Description' not in companies_info:
+            return {'error': 'Missing RoleID or Skills Required or Role Description'}, 400
+
+        company_id = companies_info['companyID']
+        role_id = companies_info['RoleID']
+        skill = companies_info['Skills Required']
+        role_description = companies_info['Role Description']
+        
+        query = '''
+        UPDATE Role
+        SET description = %s,
+            skillsRequired = %s
+        WHERE companyID = %s AND roleID = %s;
+        '''
+        
+        data = (role_description, skill, company_id, role_id)
+
+        cursor = db.get_db().cursor()
+        cursor.execute(query, data)
+        db.get_db().commit()
+
+        return {'message': 'Roles updated successfully'}, 200
+
+    except KeyError as e:
+        current_app.logger.error(f"Missing key in request JSON: {str(e)}")
+        return {'error': f'Missing key: {str(e)}'}, 400
+    except Exception as e:
+        current_app.logger.error(f"Error updating roles: {str(e)}")
+        return {'error': 'An error occurred while updating roles'}, 500
+    finally:
+        if 'cursor' in locals() and cursor:
+            cursor.close()
+
