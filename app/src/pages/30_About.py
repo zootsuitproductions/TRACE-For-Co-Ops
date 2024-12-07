359d217c3ae51eb25ff5360e38703fc7c813f837
zootsuitproductions <dannysantanasf@gmail.com>
2024-12-07 09:54:06 -0500

    fixed about page

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -2,17 +2,17 @@ import streamlit as st
 from streamlit_extras.app_logo import add_logo
 from modules.nav import SideBarLinks
 
+
 SideBarLinks()
+
+st.sidebar.page_link("Home.py", label="Home", icon="🏠")
 
 st.write("# About this App")
 
+
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
+    COUPE is a data-driven platform designed to revolutionize the Co-Op search process for Northeastern students by providing peer-to-peer insights into specific company experiences. While platforms like NUWorks provide job listings, they often fall short in detailing the real day-to-day life, culture, and interview dynamics students will encounter. COUPE fills this gap by giving students a space to share and access honest feedback about what it’s truly like working for various employers—covering everything from workplace culture to interview formats and application tips.
 
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
-
-    Stay tuned for more information and features to come!
     """
         )
