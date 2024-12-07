d1d7c6f33c81ac07ac4734ea85e6c1ae76858015
CChristinaFu <fu.chr@northeastern.edu>
2024-12-07 04:37:29 -0500

    changes

--- app/src/Home.py
+++ app/src/Home.py
@@ -1,11 +1,14 @@
 ##################################################
-# This is the main/entry-point file for the 
+# This is the main/entry-point file for the
 # sample application for your project
 ##################################################
 
 # Set up basic logging infrastructure
 import logging
-logging.basicConfig(format='%(filename)s:%(lineno)s:%(levelname)s -- %(message)s', level=logging.INFO)
+
+logging.basicConfig(
+    format="%(filename)s:%(lineno)s:%(levelname)s -- %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 # import the main streamlit library as well
@@ -15,15 +18,15 @@ from modules.nav import SideBarLinks
 
 # streamlit supports reguarl and wide layout (how the controls
 # are organized/displayed on the screen).
-st.set_page_config(layout = 'wide')
+st.set_page_config(layout="wide")
 
-# If a user is at this page, we assume they are not 
+# If a user is at this page, we assume they are not
 # authenticated.  So we change the 'authenticated' value
-# in the streamlit session_state to false. 
-st.session_state['authenticated'] = False
+# in the streamlit session_state to false.
+st.session_state["authenticated"] = False
 
 # Use the SideBarLinks function from src/modules/nav.py to control
-# the links displayed on the left-side panel. 
+# the links displayed on the left-side panel.
 # IMPORTANT: ensure src/.streamlit/config.toml sets
 # showSidebarNavigation = false in the [client] section
 SideBarLinks(show_home=True)
@@ -32,52 +35,55 @@ SideBarLinks(show_home=True)
 #    The major content of this page
 # ***************************************************
 
-# set the title of the page and provide a simple prompt. 
+# set the title of the page and provide a simple prompt.
 logger.info("Loading the Home page of the app")
-st.title('TRACE For Co Ops')
-st.write('\n\n')
-st.write('### HI! As which user would you like to log in?')
+st.title("Welcome to COUPE")
+st.write("\n\n")
+st.write("### HI! As which user would you like to log in?")
 
 # For each of the user personas for which we are implementing
-# functionality, we put a button on the screen that the user 
-# can click to MIMIC logging in as that mock user. 
+# functionality, we put a button on the screen that the user
+# can click to MIMIC logging in as that mock user.
 
-if st.button("Act as Sebastian, a Student Co-Op Searcher", 
-            type = 'primary', 
-            use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'Co-Op searcher'
-    st.session_state['first_name'] = 'Sebastian'
-    st.session_state['id'] = '5'
+if st.button(
+    "Act as Sebastian, a Student Co-Op Searcher",
+    type="primary",
+    use_container_width=True,
+):
+    st.session_state["authenticated"] = True
+    st.session_state["role"] = "Co-Op searcher"
+    st.session_state["first_name"] = "Sebastian"
+    st.session_state["id"] = "5"
     logger.info("Logging in as Co-Op Searcher")
-    st.switch_page('pages/100_CoOp_Searcher_Home.py')
+    st.switch_page("pages/100_CoOp_Searcher_Home.py")
 
-if st.button('Act as Riley, a student Co-Op Reviewer', 
-            type = 'primary', 
-            use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'Co-Op reviewer'
-    st.session_state['id'] = '1'
-    st.session_state['first_name'] = 'Riley'
+if st.button(
+    "Act as Riley, a student Co-Op Reviewer", type="primary", use_container_width=True
+):
+    st.session_state["authenticated"] = True
+    st.session_state["role"] = "Co-Op reviewer"
+    st.session_state["id"] = "1"
+    st.session_state["first_name"] = "Riley"
 
     logger.info("Logging in as Co Op Reviewer")
-    st.switch_page('pages/200_CoOp_Reviewer_Home.py')
+    st.switch_page("pages/200_CoOp_Reviewer_Home.py")
 
-if st.button('Act as Alex Admin, System Administrator', 
-            type = 'primary', 
-            use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'administrator'
-    st.session_state['first_name'] = 'Alex'
+if st.button(
+    "Act as Alex Admin, System Administrator", type="primary", use_container_width=True
+):
+    st.session_state["authenticated"] = True
+    st.session_state["role"] = "administrator"
+    st.session_state["first_name"] = "Alex"
     logger.info("Logging in as Alex Admin, System Administrator")
-    st.switch_page('pages/300_System_Administrator_Home.py')
+    st.switch_page("pages/300_System_Administrator_Home.py")
 
 
-if st.button('Act as Annalise, an Analyist of Site Performance', 
-            type = 'primary', 
-            use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'analyst'
-    st.session_state['first_name'] = 'Annalise'
-    st.switch_page('pages/400_Analyst_Home.py')
-
+if st.button(
+    "Act as Annalise, an Analysist of Site Performance",
+    type="primary",
+    use_container_width=True,
+):
+    st.session_state["authenticated"] = True
+    st.session_state["role"] = "administrator"
+    st.session_state["first_name"] = "SysAdmin"
+    st.switch_page("pages/20_Admin_Home.py")
